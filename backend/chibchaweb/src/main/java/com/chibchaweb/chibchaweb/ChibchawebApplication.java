2e8a6e6c212835e195844788e050281e550c8267
David Marin <dfermarinlancheros@gmail.com>
2026-05-23 13:49:54 -0500

    se inicializa backend

---
+++ backend/chibchaweb/src/main/java/com/chibchaweb/chibchaweb/ChibchawebApplication.java
@@ -0,0 +1,13 @@
+package com.chibchaweb.chibchaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChibchawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChibchawebApplication.class, args);
+	}
+
+}
---
+++ backend/chibchaweb/src/test/java/com/chibchaweb/chibchaweb/ChibchawebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chibchaweb.chibchaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChibchawebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
